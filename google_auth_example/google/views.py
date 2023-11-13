bd581ba77703ea2ed5506cdc2b5381aeb0c2ce94
Ben Schreiber <bpschreiber2003@gmail.com>
2023-11-13 15:02:15 -0800

    Django google auth

---
+++ google_auth_example/google/views.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
+from rest_framework.utils import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import requests
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers
+from rest_framework import permissions
+
+class HelloWorldView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response(data={"hello": "world"}, status=200)
+
+
+class GoogleViewSerializer(serializers.Serializer):
+    token = serializers.CharField(max_length=255)
+
+class GoogleView(APIView):
+
+    @swagger_auto_schema(request_body=GoogleViewSerializer)
+    def post(self, request):
+        payload = {'access_token': request.data.get("token")}  # validate the token
+        r = requests.get('https://www.googleapis.com/oauth2/v2/userinfo', params=payload)
+        data = json.loads(r.text)
+
+        if 'error' in data:
+            content = {'message': 'wrong google token / this google token is already expired.'}
+            return Response(content)
+
+        # create user if not exist
+        try:
+            user = User.objects.get(email=data['email'])
+        except User.DoesNotExist:
+            user = User()
+            user.username = data['email']
+            # provider random default password
+            user.password = make_password(BaseUserManager().make_random_password())
+            user.email = data['email']
+            user.save()
+
+        token = RefreshToken.for_user(user)  # generate token without username & password
+        response = {}
+        response['username'] = user.username
+        response['access_token'] = str(token.access_token)
+        response['refresh_token'] = str(token)
+        return Response(response)
---
+++ google_auth_example/google_auth_example/urls.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from google import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("google/", views.GoogleView.as_view(), name="google"),
+    path("hello/", views.HelloWorldView.as_view(), name="hello"),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
